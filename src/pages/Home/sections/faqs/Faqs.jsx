ce0f4c2205f1c4af17bf88c5c7b7c9ef171899e3
BrianZenhom <contact@brianzenhom.dev>
2023-10-12 19:45:20 +0200

    ADD FAQS

--- src/pages/Home/sections/faqs/Faqs.jsx
+++ src/pages/Home/sections/faqs/Faqs.jsx
@@ -3,24 +3,41 @@ import Accordion from './../../../../components/accordion/Accordion';
 
 const data = [
   {
-    question: '¿Cuándo agregan más camisetas?',
+    question: '¿Cuánto tarda en llegar el pedido?',
     answer:
-      'Es un proyecto de un grupo con una visión, de crear una marca de camisetas, que represente bien la autenticidad de la persona. Inspira a ser uno mismo, y fiel a sus valores. Manteniendo siempre como prioridad el amor propio.',
+      `España:
+      Entre 1 y 3 días laborales.
+      
+      Unión Europea:
+      ¡Todavía no llegamos! Somos como un cachorro de tigre, lindos y peligrosos.
+
+      Internacional:
+      No realizamos envíos internacionales.
+      `,
   },
   {
-    question: '¿Cuándo llega mi pedido?',
+    question: '¿Puedo realizar la devolución?',
     answer:
-      'Es un proyecto de un grupo con una visión, de crear una marca de camisetas, que represente bien la autenticidad de la persona. Inspira a ser uno mismo, y fiel a sus valores. Manteniendo siempre como prioridad el amor propio.',
+      `¡Pero claro que si! Siempre y cuando sea dentro de los primeros 14 días naturales desde la recepción del pedido, el producto debe estar con su embalaje original, sus etiquetas y sin ningún tipo de daño o modificación de su estado inicial. Pero te adelantamos que te encantará, ¡No querrás devolverlo!`,
   },
   {
-    question: '¿Qué es ARAUCA?',
+    question: '¿Cómo localizo mi pedido?',
     answer:
-      'Es un proyecto de un grupo con una visión, de crear una marca de camisetas, que represente bien la autenticidad de la persona. Inspira a ser uno mismo, y fiel a sus valores. Manteniendo siempre como prioridad el amor propio.',
+      `En el momento de la compra y que enviemos tu pedido recibirás un correo con la confirmación de compra y un número de tracking con la empresa de envíos para que puedas hacer el seguimiento cuando tú quieras`,
   },
   {
-    question: '¿Hacen envíos a toda europa?',
+    question: '¿Cuáles son las formas de pago?',
     answer:
-      'Es un proyecto de un grupo con una visión, de crear una marca de camisetas, que represente bien la autenticidad de la persona. Inspira a ser uno mismo, y fiel a sus valores. Manteniendo siempre como prioridad el amor propio.',
+      `Para nosotros tu seguridad, tu tranquilidad y bienestar, está primero. Por eso te contamos que tenemos un certificado de seguridad SLL que hace a tus datos invisibles y completamente encriptados para los “amigos de lo ajeno “. 
+      Podrás hacer el pago con tarjeta de crédito, transferencia bancaria, Apple Pay y Google Pay.
+      `,
+  },
+  {
+    question: '¿Cuál es mi talla?',
+    answer:
+      `Todavía no nos conocemos muy bien, pero te adelantamos que en nuestras fotos encontrarás a los modelos con su altura y la talla que está usando.
+      Si todavía te quedan dudas sobre la talla, que no sea por dudar en consultar con nosotros en atención@arauca.eu, te vamos a tratar de ayudar en todo lo que podamos! Y si aún así querés estar seguro, tenemos nuestro medidor de talles en cada producto y en el medidor de talles (linkear hipervínculo a medidor de talla), pero te avisamos que puede variar un poco estas medidas.
+      `,
   },
 ];
 
