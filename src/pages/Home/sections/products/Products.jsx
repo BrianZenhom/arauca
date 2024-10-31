0c9ca285eb9149ac67d831f253cf8d65c5d106e1
brianzenhom <brianzenhom@hotmail.com>
2024-10-31 17:46:18 +0100

    Update readme

--- src/pages/Home/sections/products/Products.jsx
+++ src/pages/Home/sections/products/Products.jsx
@@ -20,7 +20,7 @@ export default function Products() {
     })
   }
 
-  const filteredTshirts = filterProducts(tshirtCollection)
+  const products = filterProducts(tshirtCollection)
 
   const handleOpenFilters = () => {
     setFiltersOpen(!filtersOpen)
@@ -41,40 +41,7 @@ export default function Products() {
           <PlusSign /> Filtro
         </button>
       </div>
-      <article className="products_content container ">
-        {filteredTshirts.map(t => {
-          return (
-            <>
-              <div className="products_content-card" key={t.Id}>
-                <div className="products_content-card">
-                  <div className="img_box">
-                    <img
-                      style={{
-                        width: '100%',
-                        height: '100%',
-                        aspectRatio: 938 / 1067,
-                      }}
-                      className="image"
-                      src={t.img}
-                      alt={t.title}
-                    />
-                  </div>
-                </div>
-                <div className="products_description">
-                  <div className="products_details">
-                    <span>Arauca</span>
-                    <span className="products_name">{t.title}</span>
-                    <span className="products_price">{t.price},00 €</span>
-                  </div>
-                  <button>
-                    <PlusSign /> Añadir
-                  </button>
-                </div>
-              </div>
-            </>
-          )
-        })}
-      </article>
+      <Products products={products} />
       <div className="singleCard">
         <div className="singleCard_content">
           <div className="singleCard_wrapper">
