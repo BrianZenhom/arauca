06f87790b77672d2826247ca9bd18b23b1c0f8e6
brianzenhom <brianzenhom@hotmail.com>
2024-10-31 17:13:36 +0100

    Add json for products

---
+++ src/services/tshirts.js
@@ -0,0 +1,28 @@
+import tshirtTest from '/araucav1.webp'
+import tshirtTest2 from '/araucav2.webp'
+import tshirtTest5 from '/araucav1model.webp'
+
+export const tshirtCollection = [
+  {
+    Id: 1,
+    img: tshirtTest,
+    title: 'Washed sand',
+    price: 20,
+    category: 'all',
+  },
+
+  {
+    Id: 2,
+    img: tshirtTest2,
+    title: 'Washed dark',
+    price: 15,
+    category: 'all',
+  },
+  {
+    Id: 3,
+    img: tshirtTest5,
+    title: 'Washed khaki',
+    price: 15,
+    category: 'all',
+  },
+]
