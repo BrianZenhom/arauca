8f0bd459b24446a4b95480e98e5f6f77c079f778
brianzenhom <brianzenhom@hotmail.com>
2024-11-05 15:02:22 +0100

    Add Cart provider

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,15 +4,16 @@ import Navbar from './../../components/navbar/Navbar'
 import NewsLetter from './sections/newsletter/NewsLetter'
 import Faqs from './sections/faqs/Faqs'
 import Footer from './../../components/footer/Footer'
+import { CartProvider } from '../../context/cart'
 
 export default function Home() {
   return (
-    <>
+    <CartProvider>
       <Navbar />
       <Products />
       <NewsLetter />
       <Faqs />
       <Footer />
-    </>
+    </CartProvider>
   )
 }
