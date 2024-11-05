7a2d2ced8e1eb521a46f40b867ce1ad3a747b080
brianzenhom <brianzenhom@hotmail.com>
2024-11-05 15:02:05 +0100

    Add filters

--- src/context/filters.jsx
+++ src/context/filters.jsx
@@ -1,9 +1,7 @@
 import { createContext, useState } from 'react'
 
-// Crear contexto
 export const FiltersContext = createContext()
 
-// Crear el provider, para proveer el contexto.
 export function FiltersProvider({ children }) {
   const [filters, setFilters] = useState({
     category: 'all',
