0d172156188f5c672212a1dc97d1cd884f8e02fa
brianzenhom <brianzenhom@hotmail.com>
2024-11-11 16:54:07 +0100

    Create useScroll hook

---
+++ src/hooks/useScroll.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react'
+
+export const useScroll = () => {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  const handleScroll = () => {
+    if (window.scrollY > 60) {
+      setIsScrolled(true)
+    } else {
+      setIsScrolled(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true })
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  return isScrolled
+}
