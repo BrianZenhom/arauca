c3a8d5e64235fbbcc86ae42807e93fd74d4fa296
brianzenhom <brianzenhom@hotmail.com>
2024-11-13 10:36:31 +0100

    Change context provider

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,19 +4,18 @@ import Navbar from './../../components/navbar/Navbar'
 import NewsLetter from './sections/newsletter/NewsLetter'
 import Faqs from './sections/faqs/Faqs'
 import Footer from './../../components/footer/Footer'
-import { CartProvider } from '../../context/cart'
 import { TestFooter } from './../../components/TestFooter'
 import { IS_DEVELOPMENT } from '../../../config'
 
 export default function Home() {
   return (
-    <CartProvider>
+    <>
       <Navbar />
       <Products />
       <NewsLetter />
       <Faqs />
       <Footer />
       {IS_DEVELOPMENT && <TestFooter />}
-    </CartProvider>
+    </>
   )
 }
