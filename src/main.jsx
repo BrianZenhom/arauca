d6560e8144a004b650396eba9a1e4b05956c85ff
brianzenhom <brianzenhom@hotmail.com>
2024-11-13 10:35:34 +0100

    Add cart context

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { FiltersProvider } from './context/filters.jsx'
+import { CartProvider } from './context/cart.jsx'
 
 const root = ReactDOM.createRoot
 
 root(document.getElementById('root')).render(
   <FiltersProvider>
-    <App />
+    <CartProvider>
+      <App />
+    </CartProvider>
   </FiltersProvider>
 )
